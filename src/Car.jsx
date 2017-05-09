402e2fbd739712db300cc37bb2e7a9de28d7f826
John Daley <johndaley.me@gmail.com>
2017-05-08 22:30:59 -0400

    Add test to check child component

--- src/Car.jsx
+++ src/Car.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Engine from './Engine';
 
 // testing hint: export the named render function for unit testing
 // as the default exported component at the end may be a Higher Order Component
@@ -8,6 +9,7 @@ export const Car = ({ color }) => (
   <div>
     {/* Find and test dynamic data by id */}
     <h1 id='car--heading'>I am a {color} car</h1>
+    <Engine type='diesel' />
   </div>
 );
 Car.propTypes = {
--- test/Car.spec.js
+++ test/Car.spec.js
@@ -2,17 +2,28 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import { Car } from '../src/Car';
+import Engine from '../src/Engine';
 
 describe('<Car />', function () {
-  describe('renders data', function () {
+  describe('renders', function () {
     let wrapper;
     before(function () {
       wrapper = shallow(<Car color='red' />);
     });
+    // check dynamic data renders correctly
+    // find elements by ID
     it('renders the heading', function () {
       const heading = wrapper.find('#car--heading');
       expect(heading).to.have.lengthOf(1);
       expect(heading.text()).to.equal('I am a red car');
+    });
+    // check child components are called correctly
+    it('has a diesel engine', function () {
+      // testing hint: If the child component is a composed component
+      // then you will likely need to find by class otherwise
+      // finding by string (e.g. 'Engine') will work
+      const engine = wrapper.find(Engine);
+      expect(engine.prop('type')).to.equal('diesel');
     });
   });
 });
